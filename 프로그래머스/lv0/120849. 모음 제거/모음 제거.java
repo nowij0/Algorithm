4ff4c06efa9abb08823846008bd7759df0bda456
Jiwon <118253722+nowij0@users.noreply.github.com>
2023-04-14 15:43:52 +0900

    [level 0] Title: 모음 제거, Time: 0.16 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -1,16 +1,9 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        String[] vowels = {"a","e","i","o","u"};
-        
-        for(int i=0; i < vowels.length; i++){
-            if(my_string.contains(vowels[i])){
-                answer = my_string.replace(vowels[i], "");
-                my_string = answer;
-            } else {
-                answer = my_string;
-            }
-        }
+
+        answer = my_string.replaceAll("[aeiou]", "");
+
         return answer;
     }
 }
