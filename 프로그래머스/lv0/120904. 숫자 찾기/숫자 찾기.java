3e24f62602b05946b68d584f804273bbb857a57d
Jiwon <118253722+nowij0@users.noreply.github.com>
2023-05-16 19:50:46 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,13 +1,14 @@
 class Solution {
     public int solution(int num, int k) {
+        //int 값 String으로 바꾸고, indexOf 사용
         String strNum = Integer.toString(num);
         String strK = Integer.toString(k);
         
         int answer = strNum.indexOf(strK);
-        if(answer == -1){
+        if(answer == -1){ //해당하는 값이 strNum에 없으면 -1을 반환
             return answer;
-        } else {
-            return answer+1;   
+        } else { //indexOf는 index값 반환하니까 +1 해주기
+            return answer + 1;   
         }
     }
 }
