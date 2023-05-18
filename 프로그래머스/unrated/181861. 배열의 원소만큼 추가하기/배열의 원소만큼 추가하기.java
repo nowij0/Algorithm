1b99f3f44b24e8e05d42028f1e8618df4b410d3a
Jiwon <118253722+nowij0@users.noreply.github.com>
2023-05-18 17:05:39 +0900

    Update 배열의 원소만큼 추가하기.java

--- 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.java
+++ 프로그래머스/unrated/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.java
@@ -2,13 +2,13 @@ import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int[] arr) {
-        ArrayList<Integer> temp = new ArrayList();
+        ArrayList<Integer> temp = new ArrayList(); //arrayList로 배열 만들어서 값 넣어주고
         for(int i=0; i<arr.length; i++){
             for(int j=0; j<arr[i]; j++){
                 temp.add(arr[i]);   
             }
         }
-        
+        //arrayList로 크기만큼 int[] 만들어서 get()으로 넣어주기
         int[] answer = new int[temp.size()];
         for(int i=0; i<temp.size(); i++){
             answer[i] = temp.get(i);
