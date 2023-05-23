c556023c8c2f3d8332b5077a5030ffe364fbd05a
Jiwon <118253722+nowij0@users.noreply.github.com>
2023-05-23 15:56:46 +0900

    Update 2차원으로 만들기.java

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[][] solution(int[] num_list, int n) {
-        int column = num_list.length/n;
-        int[][] answer = new int[column][n];
+        int column = num_list.length / n;
+        int[][] answer = new int[column][n]; //2차원 배열 생성
         
-        int num = 0;
-        for(int i=0; i<column; i++){
-            for(int j=0; j<n; j++){
+        int num = 0; //num_list의 index값
+        for(int i=0; i<column; i++){ //행만큼 반복
+            for(int j=0; j<n; j++){ //열만큼 반복 
                 answer[i][j] = num_list[num];
-                num++;
+                num++; //num_list의 index 값 1씩 더해주기
             }
         }
         return answer;
