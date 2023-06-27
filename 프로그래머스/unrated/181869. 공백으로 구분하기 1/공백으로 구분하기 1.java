441ff21dec46224528ea43d409436bb044e9855e
Jiwon <118253722+nowij0@users.noreply.github.com>
2023-06-27 12:07:50 +0900

    [unrated] Title: 공백으로 구분하기 1, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -1,14 +1,6 @@
 class Solution {
     public String[] solution(String my_string) {
-        int cnt = (int)my_string.chars().filter(e->e == ' ').count();
-        String[] answer = new String[cnt];   
-        if(cnt == 0){
-            answer = new String[]{my_string};
-            return answer;
-        } 
-        for(int i=0; i<answer.length; i++){
-            answer = my_string.split(" ");
-        }
+        String[] answer = my_string.split(" ");
         return answer;
     }
 }
