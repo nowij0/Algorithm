c877ef28ae4c01ca0eb09c71a2d7c8955c077acc
Jiwon <118253722+nowij0@users.noreply.github.com>
2024-01-02 20:39:13 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,20 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+		
+		int a = 0;
+		int b = 0;
+		
+		System.out.print("");
+		a = sc.nextInt();
+		System.out.print("");
+		b = sc.nextInt();
+		
+		System.out.println(a+b);
+		
+	}
+
+}
