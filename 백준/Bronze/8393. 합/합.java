e3bcb00ed50008854a98cb06d8f2c590f650ca0a
Jiwon <118253722+nowij0@users.noreply.github.com>
2024-01-08 12:55:24 +0900

    [Bronze V] Title: 합, Time: 120 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        int result = 0;
+        for(int i=1; i < num + 1; i++){
+            result += i;
+        }
+        System.out.println(result);
+    }
+}
